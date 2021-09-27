4b00e88c9827e0103aabf5dbe4c7bed27044098b
ilj3 <ilj3@duke.edu>
2021-09-27 12:20:28 -0400

    attempt

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,10 +1,55 @@
 #include "pandemic.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 country_t parseLine(char * line) {
-  //WRITE ME
+  uint64_t population;
   country_t ans;
-  ans.name[0] = '\0';
-  ans.population = 0;
+  country_t failure;
+  failure.name[0] = '\0';
+  failure.population = 0;
+
+  // FILL NAME
+  for (int i = 0; i < 64; i++) {
+    if ((*line) == ',') {
+      break;
+    }  // stop if we've reached population
+
+    else if (line == NULL) {  // check we haven't reached end of line
+      fprintf(stderr, "Not a valid line.");
+      return failure;
+    }
+
+    else {  // otherwise fill up name box
+      ans.name[i] = *line;
+    }
+    line++;  // point to next character
+  }
+
+  if (*line != ',') {  // if we've gone through 64 chars but no ',' then name is too long
+    fprintf(stderr, "Name too long!");
+    return failure;
+  }
+
+  // FILL POPULATION
+  line++;                                     // skip the comma
+  const char * population_str = line;         // create container for population
+  int population_int = atoi(population_str);  // convert to int
+  if (population_int < 0) {                   // check for non-zero
+    fprintf(stderr, "Population must be non-zero.");
+    return failure;
+  }
+
+  else if (population_int > (pow(2, 64) - 1)) {  // check for size
+    fprintf(stderr, "Population too large.");
+    return failure;
+  }
+
+  else {
+    population = (uint64_t)population_int;  // cast to proper type
+    ans.population = population;
+  }
   return ans;
 }
 
