6c4489f0857928765cc1232afc07eb39483cd21f
ilj3 <ilj3@duke.edu>
2021-10-09 18:43:19 -0400

    attempt

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,18 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char * outputName;
+  size_t fullsize = sizeof(*inputName) * strlen(inputName) +
+                    8 * (sizeof(outputName));  // calculate fullsize of output
+
+  outputName = malloc(fullsize);
+  outputName = strcpy(outputName, inputName);
+  char * nullIndex = strchr(outputName, '\0');  // overwrite the original null terminator
+  char * suffix = ".counts";  // this syntax should auto add a null terminator
+  strcpy(nullIndex, suffix);
+  return outputName;
 }
