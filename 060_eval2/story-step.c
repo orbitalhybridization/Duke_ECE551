c1f82c1142ea91fc907cc42c82b67591d5fe5ba3
ilj3 <ilj3@duke.edu>
2021-10-13 17:35:39 -0400

    attempt

---
+++ 060_eval2/story-step.c
@@ -0,0 +1,17 @@
+
+void parseUnderscores();
+
+int main(int argc, char ** argv) {
+  // error check num command line args
+
+  // open file for read
+
+  // error check file opened correctly
+
+  // read line by line
+
+  // go through each line and parse the underscores
+  // this should be abstracted into a separate function
+
+  // close file
+}
