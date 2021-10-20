cf619ba93ecfc48d2b91a1b5ce4afe99ebe2316c
ilj3 <ilj3@duke.edu>
2021-10-20 14:21:50 -0400

    attempt

--- 069_point/point.hpp
+++ 069_point/point.hpp
@@ -9,12 +9,7 @@ class Point {
   // default constructor
   Point() : x(0), y(0) {}
 
-  void move(double dx, double dy) {
-    x += dx;
-    y += dy;
-  }
+  void move(double dx, double dy);
 
-  double distanceFrom(const Point & p) {
-    return std::sqrt(std::pow((this->x - p.x), 2) + std::pow((this->y - p.y), 2));
-  }
+  double distanceFrom(const Point & p);
 };
