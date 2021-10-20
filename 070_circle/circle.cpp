1037385a582b6b5ada3bb84bb37edaee636f26b2
ilj3 <ilj3@duke.edu>
2021-10-20 14:59:57 -0400

    att

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -1,5 +1,6 @@
 #include "circle.hpp"
 
+#include <stdint.h>
 #include <stdlib.h>
 
 #include <cmath>
@@ -11,15 +12,35 @@ void Circle::move(double dx, double dy) {
 
 double Circle::intersectionArea(const Circle & otherCircle) {
   // calculate the intersection area between this circle and other
+
+  // test case: no intersection
+
+  // test case: they are the same circle
+
+  // test case: one inside the other
   double r_sq = std::pow(otherCircle.radius, 2);
-  double R_sq = std::pow(this->radius, 2);
+  double R_sq = std::pow(radius, 2);
   double r = otherCircle.radius;
-  double R = this->radius;
-  double d = this->center.distanceFrom(otherCircle.center);
+  double R = radius;
+  double d = center.distanceFrom(otherCircle.center);
+  if ((d == 0) && (R == r)) {  // test case the are the same
+    return M_PI * R_sq;
+  }
+
+  if ((d > r) && (d > R)) {  // test case: no intersection
+    return 0;
+  }
+  if (d < r) {
+    return M_PI * r_sq;
+  }
+
+  if (d < R) {
+    return M_PI * R_sq;
+  }
   double d_sq = std::pow(d, 2);
   double area =
-      r_sq * std::acos((d_sq + r_sq - R_sq) / 2 * d * r) +
-      R_sq * std::acos((d_sq - r_sq + R_sq) / 2 * d * R) -
+      r_sq * std::acos((d_sq + r_sq - R_sq) / (2 * d * r)) +
+      R_sq * std::acos((d_sq - r_sq + R_sq) / (2 * d * R)) -
       0.5 * std::pow((d + r - R) * (d - r + R) * (-d + r + R) * (d + r + R), 0.5);
   return area;
 }
