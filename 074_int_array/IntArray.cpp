b937d07971cf22e5eb6c796beedca49084037349
ilj3 <ilj3@duke.edu>
2021-10-22 00:19:35 -0400

    attempt

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -78,10 +78,15 @@ bool IntArray::operator!=(const IntArray & rhs) const {
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-  s << "{";
-  for (int i = 0; i < rhs.size() - 1; i++) {
-    s << rhs[i] << ", ";
+  if (rhs.size() == 0) {
+    s << "{}";
   }
-  s << rhs[rhs.size() - 1] << "}";
+  else {
+    s << "{";
+    for (int i = 0; i < rhs.size() - 1; i++) {
+      s << rhs[i] << ", ";
+    }
+    s << rhs[rhs.size() - 1] << "}";
+  }
   return s;
 }
