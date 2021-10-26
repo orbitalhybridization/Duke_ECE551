9c993b541fe5753832c79c9a8afe721432f98d94
ilj3 <ilj3@duke.edu>
2021-10-26 18:23:16 -0400

    att[

---
+++ 084_expr_eval2/expr.hpp
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+class Expression {
+ public:
+  virtual std::string toString() const = 0;  // abstract method
+  virtual ~Expression(){};
+};
+
+class NumExpression : public Expression {
+ private:
+  long num;
+
+ public:
+  NumExpression(long input_num) : num(input_num){};
+
+  virtual std::string toString() const {
+    std::stringstream out;  // container
+    out << num;             // put num in string stream
+    return out.str();       // cast to string and return
+  }
+};
+
+class PlusExpression : public Expression {
+ private:
+  Expression * left_op;
+  Expression * right_op;
+
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : left_op(lhs), right_op(rhs){};
+
+  virtual std::string toString() const {
+    std::stringstream out;  // container
+    out << "(";
+    out << left_op->toString();
+    out << " + ";
+    out << right_op->toString();
+    out << ")";
+    return out.str();  // cast to string and return
+  }
+  virtual ~PlusExpression() {
+    delete left_op;  // free memory
+    delete right_op;
+  }
+};
+
+class MinusExpression : public Expression {
+ private:
+  Expression * left_op;
+  Expression * right_op;
+
+ public:
+  MinusExpression(Expression * lhs, Expression * rhs) : left_op(lhs), right_op(rhs){};
+  virtual std::string toString() const {
+    std::stringstream out;  // container
+    out << "(";
+    out << left_op->toString();
+    out << " - ";
+    out << right_op->toString();
+    out << ")";
+    return out.str();  // cast to string and return
+  }
+  virtual ~MinusExpression() {
+    delete left_op;
+    delete right_op;
+  }
+};
+
+class TimesExpression : public Expression {
+ private:
+  Expression * left_op;
+  Expression * right_op;
+
+ public:
+  TimesExpression(Expression * lhs, Expression * rhs) : left_op(lhs), right_op(rhs){};
+  virtual std::string toString() const {
+    std::stringstream out;  // container
+    out << "(";
+    out << left_op->toString();
+    out << " * ";
+    out << right_op->toString();
+    out << ")";
+    return out.str();  // cast to string and return
+  }
+
+  virtual ~TimesExpression() {
+    delete left_op;
+    delete right_op;
+  }
+};
+
+class DivExpression : public Expression {
+ private:
+  Expression * left_op;
+  Expression * right_op;
+
+ public:
+  DivExpression(Expression * lhs, Expression * rhs) : left_op(lhs), right_op(rhs){};
+
+  virtual std::string toString() const {
+    std::stringstream out;  // container
+    out << "(";
+    out << left_op->toString();
+    out << " / ";
+    out << right_op->toString();
+    out << ")";
+    return out.str();  // cast to string and return
+  }
+  virtual ~DivExpression() {
+    delete left_op;
+    delete right_op;
+  }
+};
