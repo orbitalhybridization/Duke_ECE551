bca0c898ee9325dc341fe243d421cf5565d45ff4
ilj3 <ilj3@duke.edu>
2021-11-17 14:13:35 -0500

    attempt

--- 093_eval3/foo.hpp
+++ 093_eval3/foo.hpp
@@ -8,6 +8,41 @@
 #include <string>
 #include <vector>
 
+int validateStrInt(const char * page) {
+  // check if a page number is a valid integer
+  // based off of code from (man strtol(3))
+  // return number, or exit failure
+  // (reused from eval 2)
+  char * endptr;
+  long as_long = strtol(page, &endptr, 10);
+
+  // if we don't have a number at all
+  if (endptr == page) {
+    //return 0;
+    std::cerr << "Page number given not a number." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // error checking specifically for valid int
+  if ((as_long <= 0) || (as_long >= ((pow(2, 8 * sizeof(int)) / 2) - 1))) {
+    std::cerr << "Requested nunber " << as_long << " not a valid integer." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // error checking from man strtol(3)
+  // check if outside of range of possible long or some other error
+  errno = 0;
+  if ((errno == ERANGE && (as_long == LONG_MAX || as_long == LONG_MIN)) ||
+      (errno != 0 && as_long == 0)) {
+    std::cerr << "Requested nunber " << as_long << " not a valid integer." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // otherwise we have a valid int, so return it as an int!
+  int return_int = as_long;
+  return return_int;
+}
+
 class Choice {
  private:
   int page_num;
@@ -28,7 +63,7 @@ class Choice {
 
     std::string page_num_s = line.substr(0, index);  // take number
     page_num =
-        validatePageNum(page_num_s.c_str());  // get the c string and try to make an int
+        validateStrInt(page_num_s.c_str());  // get the c string and try to make an int
 
     description = line.substr(index + 1,
                               line.size() - 1);  // set description (does this have \0)?
@@ -43,41 +78,6 @@ class Choice {
   friend std::ostream & operator<<(
       std::ostream & s,
       const Choice & rhs);  // overload << operator for Choice
-
-  int validatePageNum(const char * page) {
-    // check if a page number is a valid integer
-    // based off of code from (man strtol(3))
-    // return number, or exit failure
-    // (reused from eval 2)
-    char * endptr;
-    long as_long = strtol(page, &endptr, 10);
-
-    // if we don't have a number at all
-    if (endptr == page) {
-      //return 0;
-      std::cerr << "Page number given not a number." << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    // error checking specifically for valid int
-    if ((as_long <= 0) || (as_long >= ((pow(2, 8 * sizeof(int)) / 2) - 1))) {
-      std::cerr << "Requested nunber " << as_long << " not a valid integer." << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    // error checking from man strtol(3)
-    // check if outside of range of possible long or some other error
-    errno = 0;
-    if ((errno == ERANGE && (as_long == LONG_MAX || as_long == LONG_MIN)) ||
-        (errno != 0 && as_long == 0)) {
-      std::cerr << "Requested nunber " << as_long << " not a valid integer." << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    // otherwise we have a valid int, so return it as an int!
-    int return_int = as_long;
-    return return_int;
-  }
 };
 
 class Page {
@@ -147,8 +147,9 @@ class Page {
   // error checking
   // also might be std::ios_base::failure
   catch (const std::ifstream::failure & e) {  // from ifstream exceptions cppreference
-    std::cerr << e.what() << std::endl;
-    exit(EXIT_FAILURE);
+    std::cerr << "Unable to open/read " << fname << std::endl;
+    //    exit(EXIT_FAILURE);
+    throw;  // rethrow to caller (story)
   }
 
   Page(const Page & rhs) :
@@ -160,7 +161,6 @@ class Page {
 
   virtual ~Page() {  // destructor
   }
-
   std::string getText() { return text; }
   // get private field, text
 
@@ -187,17 +187,32 @@ class Story {
   Story(std::string directory) try : directory_name(directory) {
     std::string page_number_s = "1";
     int page_number_i = 1;
-    std::string page_filename = "story" + page_number_s + ".txt";
+    std::string page_filename = "story1.txt";
+    std::stringstream s;
     // read pages
-      while (validateInt(page_number_i) { // we can read pages as long as they're valid ints
-      Page new_page = Page(page_filename);
-      if (!checkPage && page_number_i == 1) {
-        std::cerr << "Page 1 doesn't exist!" << std::endl;
-        exit(EXIT_FAILURE);
+    // we can read pages as long as they're valid ints
+    while (validateStrInt(page_number_s.c_str())) {
+      Page new_page = Page(page_filename.c_str());  // create page from filename
+
+      s << page_number_i;
+      page_number_s = s.str();
+      page_filename = "story" + s.str() + ".txt";
+
+      // check if we can open
+
+      //      if (!checkPage && (page_number_i == 1)) {
+      //std::cerr << "Page 1 doesn't exist!" << std::endl;
+      //exit(EXIT_FAILURE);
+      //}
+
+      if (!checkPage) {
+        break;
       }
-      // increment page number
-      // pages.push_back(new_page);
-	}
+
+      // otherwise increment page number and add page to list
+      page_number_i++;
+      pages.push_back(new_page);
+    }
 
     validatePages();
   }
@@ -207,13 +222,38 @@ class Story {
     exit(EXIT_FAILURE);
   }
 
+  bool validatePages(){
+
+      //make sure every page referenced by a choice is valid
+
+      // every page (except 1) must be referenced by some other pages choices
+
+      // must have a win page and a los page
+
+  };
+
+  void read() {
+    //current_page = page1;
+    //display page1
+
+    // while page.win[0] == False {
+    // if WIN or LOSE exit successfully
+
+    // otherwise read a number from the user (if not a number or not a valid choice
+    // e.g. it is larger than the length of pages vector)
+    // then prompt with message "That is not a valid choice, please try again."
+    // repeat until valid choice
+
+    // current_page = cin page;
+  }
+
   ~Story(){};
-  friend std::ostream & operator<<(std::ostream & s, const Story & rhs);
+  //friend std::ostream & operator<<(std::ostream & s, const Story & rhs);
 };
 
-std::ostream & operator<<(std::ostream & s, const Story & rhs) {
-  return s;
-}
+//std::ostream & operator<<(std::ostream & s, const Story & rhs) {
+return s;
+//}
 
 std::ostream & operator<<(std::ostream & s,
                           const Page & rhs) {  // overload << operator for proper printing
