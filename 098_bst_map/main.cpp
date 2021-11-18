33c83e2ef0c19d295a9dc0103f83981150e6abbd
ilj3 <ilj3@duke.edu>
2021-11-17 22:06:36 -0500

    add other stuff

---
+++ 098_bst_map/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "bstmap.h"
+
+int main(int argc, char ** argv) {
+  BstMap<int, int> bst;
+  bst.add(1, 1);
+  bst.add(2, 1);
+  bst.add(17, 1);
+  bst.add(6, 1);
+  bst.add(-11, 1);
+  bst.add(-12, 1);
+  bst.add(18, 1);
+  bst.add(-9, 1);
+  bst.add(-5, 1);
+  bst.add(-6, 1);
+  bst.add(5, 1);
+  bst.add(9, 1);
+  bst.add(8, 1);
+  bst.remove(17);
+  bst.remove(-11);
+  std::cout << "Done" << std::endl;
+  return 0;
+}
