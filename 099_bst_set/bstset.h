c1cd1a84e49d4082ffa01bfc56d6033f8916df01
ilj3 <ilj3@duke.edu>
2021-11-30 16:26:42 -0500

    attempt

---
+++ 099_bst_set/bstset.h
@@ -0,0 +1,38 @@
+
+#include <algorithm>
+#include <vector>
+
+#include "set.h"
+
+template<typename T>
+class BstSet : public Set<T> {
+ public:
+  std::vector<T> data;
+  virtual void add(const T & key) {
+    // add me
+    data.push_back(key);
+  }
+
+  virtual bool contains(const T & key) {
+    // check me
+    typename std::vector<T>::iterator it = std::find(data.begin(), data.end(), key);
+    if (it == data.end()) {
+      return true;
+    }
+    return false;
+  }
+
+  virtual void remove(const T & key) {
+    // remove me
+    typename std::vector<T>::iterator it = std::find(data.begin(), data.end(), key);
+    if (it != data.end()) {
+      data.erase(it);
+    }
+  }
+
+  virtual ~BstSet<T>() {}
+
+  BstSet & operator=(const BstSet & rhs) {}
+
+  BstSet() {}
+};
---
+++ 099_bst_set/main.cpp
@@ -0,0 +1,9 @@
+#include <string.h>
+
+#include <iostream>
+
+#include "bstset.h"
+
+int main(int argc, char ** argv) {
+  return 0;
+}
