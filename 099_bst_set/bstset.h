58375e2d4f048c0e53a29a989dcf082ded43885f
ilj3 <ilj3@duke.edu>
2021-11-30 17:51:55 -0500

    attempt

--- 099_bst_set/bstset.h
+++ 099_bst_set/bstset.h
@@ -1,55 +1,207 @@
-
-#include <algorithm>
-#include <vector>
+#include <iostream>
+#include <stdexcept>
 
 #include "set.h"
 
 template<typename T>
 class BstSet : public Set<T> {
+ private:
+  class Node {
+   public:
+    T key;
+    Node * left;
+    Node * right;
+
+    Node(T in_data) : key(in_data), left(NULL), right(NULL) {}
+
+    ~Node() {}
+  };
+  Node * root;
+
  public:
-  std::vector<T> data;
-  virtual void add(const T & key) {
-    // add me
-    data.push_back(key);
+  BstSet() : root(NULL) {}
+
+  BstSet & operator=(const BstSet & rhs) {
+    // assignment
+    if (this != &rhs) {
+      clear(root);  // empty all nodes
+      root = NULL;
+      makeEqual(rhs.root);  // do a preorder traversal to reconstruct
+    }
+    return *this;
   }
 
+  BstSet(const BstSet & rhs) : root(NULL) {
+    // copy ctor
+    //    std::cout << "key " << rhs.root->key << std::endl;
+    makeEqual(rhs.root);  // do a preorder traversal
+  }
+
+  void clear(Node * node) {
+    // do a post order traversal
+    if (node != NULL) {
+      clear(node->left);
+      clear(node->right);
+      delete node;
+    }
+  }
+
+  void makeEqual(Node * node) {
+    if (node != NULL) {
+      add(node->key);
+      makeEqual(node->left);
+      makeEqual(node->right);
+    }
+  }
+
+  Node * add_helper(Node * current, const T & addData) {
+    if (current == NULL) {
+      Node * ans = new Node(addData);
+      return ans;
+    }
+    else {
+      if (addData < current->key) {
+        Node * newLeft = add_helper(current->left, addData);
+        current->left = newLeft;
+      }
+
+      else if (addData > current->key) {
+        Node * newRight = add_helper(current->right, addData);
+        current->right = newRight;
+      }
+
+      //else {  // same key
+
+      //}
+
+      return current;
+    }
+  }
+
+  virtual void add(const T & key) { root = add_helper(root, key); }
+
+  /*
+  virtual void add(const K & key, const V & value) {
+    Node ** curr = &root;
+    while (*curr != NULL) {
+      if (key < (*curr)->key) {
+        curr = &(*curr)->left;
+      }
+      else {
+        curr = &(*curr)->right;
+      }
+    }
+    *curr = new Node(key, value);
+  }
+  */
+
   virtual bool contains(const T & key) const {
-    // check me
-    typename std::vector<T>::const_iterator it = std::find(data.begin(), data.end(), key);
-    if (it == data.end()) {
-      return true;
+    Node * current = root;
+    while (current != NULL) {
+      if (key == current->key) {
+        return true;
+      }
+      else if (key < current->key) {
+        current = current->left;
+      }
+      else {
+        current = current->right;
+      }
     }
     return false;
   }
 
   virtual void remove(const T & key) {
-    // remove me
-    typename std::vector<T>::iterator it = std::find(data.begin(), data.end(), key);
-    if (it != data.end()) {
-      data.erase(it);
+    // keep track of parent
+    Node * parent = root;
+    Node * child = root;
+    if (root == NULL) {
+      return;
     }
-  }
-
-  virtual ~BstSet<T>() {}
-
-  virtual BstSet & operator=(const BstSet & rhs) {
-    if (this != &rhs) {
-      for (typename std::vector<T>::const_iterator it = rhs.data.begin();
-           it != rhs.data.end();
-           it++) {
-        data.push_back(*it);
+    while (child != NULL) {
+      if (child->key == key) {
+        break;  // no key found
+      }
+      // find the key that we want to remove
+      if (key < child->key) {
+        parent = child;
+        child = child->left;
+      }
+      else {
+        parent = child;
+        child = child->right;
       }
     }
-    return *this;
+
+    if (child == NULL) {
+      return;
+    }
+
+    if (child == parent->left) {
+      parent->left = remove_helper(child);
+    }
+    else if (child == parent->right) {
+      parent->right = remove_helper(child);
+    }
+
+    else {  // root
+      root = remove_helper(child);
+    }
+    // now that we've found the one to remove,
+    // we want to abort the child
   }
 
-  BstSet(const BstSet & rhs) {
-    for (typename std::vector<T>::const_iterator it = rhs.data.begin();
-         it != rhs.data.end();
-         it++) {
-      data.push_back(*it);
+  Node * remove_helper(Node * node) {
+    if (node->left == NULL) {
+      Node * temp = node->right;
+      //      std::cout << "Deleting " << node->key << std::endl;
+      delete node;
+      return temp;
+    }
+    else if (node->right == NULL) {
+      // has a left child
+      Node * temp = node->left;
+      //std::cout << "Deleting " << node->key << std::endl;
+      delete node;
+      return temp;
+    }
+    else {  // has two children
+      // go left one
+      Node * temp = node->left;
+      Node * temp_parent = node;
+      // go right as much as possible
+      while (temp->right != NULL) {
+        temp_parent = temp;
+        temp = temp->right;
+      }
+      // return
+      //      node->key = temp->key;
+      //node->value = temp->value;
+      if (temp == temp_parent->left) {
+        temp_parent->left = temp->left;
+      }
+      else {
+        temp_parent->right = temp->left;
+      }
+      Node * new_node = new Node(temp->key);
+      new_node->left = node->left;
+      new_node->right = node->right;
+
+      delete temp;
+      delete node;
+      return new_node;
     }
   }
 
-  BstSet() {}
+  virtual ~BstSet<T>() { clear(root); };
+
+  void show() { show(root); }
+
+  void show(Node * node) {
+    if (node != NULL) {
+      //      std::cout << "Node key : " << node->key << std::endl;
+      show(node->left);
+      show(node->right);
+    }
+  }
 };
--- 099_bst_set/main.cpp
+++ 099_bst_set/main.cpp
@@ -4,9 +4,135 @@
 
 #include "bstset.h"
 
+class A {
+  int x;
+
+ public:
+  A() : x(0) {}
+
+  A & operator=(const A & rhs) {
+    if (this != &rhs) {
+      x = rhs.x;
+    }
+    return *this;
+  }
+
+  A(int x_in) : x(x_in) {}
+
+  A(const A & rhs) { x = rhs.x; }
+
+  bool operator<(const A & rhs) const {
+    if (x < rhs.x) {
+      return true;
+    }
+    return false;
+  };
+
+  bool operator>(const A & rhs) const {
+    if (x > rhs.x) {
+      return true;
+    }
+    return false;
+  }
+
+  bool operator==(const A & rhs) const {
+    if (x == rhs.x) {
+      return true;
+    }
+    return false;
+  }
+
+  bool operator!=(const A & rhs) const {
+    if (x != rhs.x) {
+      return true;
+    }
+
+    return false;
+  }
+
+  ~A() {}
+};
+
 int main(int argc, char ** argv) {
-  BstSet<int> * B = new BstSet<int>();
-  B->add(1);
-  delete B;
+  BstSet<int> bst;
+  bst.add(1);
+  bst.add(2);
+  bst.add(17);
+  bst.add(6);
+  bst.add(-11);
+  bst.add(-12);
+  bst.add(18);
+  bst.add(-9);
+  bst.add(-5);
+  bst.add(-6);
+  bst.add(5);
+  bst.add(9);
+  bst.add(8);
+  bst.remove(17);
+  //  bst.show();
+  bst.remove(-11);
+  //  bst.show();
+  // test equals
+  BstSet<int> bts;
+  bts = bst;
+  bts.remove(1);
+  //  std::cout << "here1";
+  bts.add(1);
+  //std::cout << "here?";
+
+  BstSet<int> bt(bts);
+  bt.remove(8);
+  std::cout << "Done with ints" << std::endl;
+
+  BstSet<std::string> bstr;
+  bstr.add("");
+  bstr.add("s");
+  bstr.add("a");
+  bstr.add("u");
+  bstr.add("11");
+  bstr.add("--1");
+  bstr.add("aefre");
+  bstr.add("w");
+  bstr.add("tt");
+  bstr.add("q");
+  bstr.add("i03");
+  bstr.add("\0");
+  bstr.add("10");
+  bstr.show();
+  bstr.remove("--1");
+
+  BstSet<std::string> btstr;
+  btstr = bstr;
+  btstr.remove("");
+  //  std::cout << "here1";
+  btstr.add("");
+  //std::cout << "here?";
+
+  BstSet<std::string> bt_(btstr);
+  bt_.remove("tt");
+  bt_.remove("not in list");
+
+  BstSet<const char *> bst_ch;
+  bst_ch.add("q");
+  bst_ch.add("t");
+
+  BstSet<const char *> bst_ch2 = bst_ch;
+  bst_ch.remove("q");
+  std::cout << "Done with strs" << std::endl;
+
+  // create a new class!
+  A a;
+  // create a map w/ two of em
+  BstSet<A> bst_A;
+  const A a1 = A(1);
+  const A a2 = A(2);
+  const A a3 = A(3);
+  const A a4 = A(4);
+  bst_A.add(a1);
+  bst_A.add(a3);
+  // try remove
+  bst_A.remove(a1);
+  // profit.
+  std::cout << "Done with classtype" << std::endl;
   return 0;
 }
