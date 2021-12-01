5a98e938ddc52d6d72f35420cb597a5401ad85b6
ilj3 <ilj3@duke.edu>
2021-12-01 14:31:12 -0500

    attempt

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,28 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+Node * buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));  // push non-zeros into priority queue
+    }
+  }
+
+  while (pq.size() != 1) {
+    // build tree
+    Node * p1 = pq.top();  // pop first two
+    pq.pop();
+    Node * p2 = pq.top();
+    pq.pop();
+
+    // make a parent, sum of child freq
+    Node * parent = new Node(p1, p2);
+    // enqueue parent
+    pq.push(parent);
+  }
+
+  // return full tree
+  Node * root = pq.top();
+  pq.pop();
+  return root;
+}
