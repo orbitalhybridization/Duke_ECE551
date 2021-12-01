d0386eb1ff09f41a50238ef23ea80a1b1c687193
ilj3 <ilj3@duke.edu>
2021-12-01 15:05:24 -0500

    Attempt

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -2,13 +2,12 @@
 
 #include <cassert>
 void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
-  if (sym != NO_SYM) {
+  if (sym == NO_SYM) {
     left->buildMap(b.plusZero(), theMap);  // recurse left
     right->buildMap(b.plusOne(), theMap);  // recurse right
   }
   else {  // leaf node
-    //    assert((left == NULL) && (right == NULL));
-
+    assert((left == NULL) && (right == NULL));
     theMap.insert(
         std::pair<unsigned, BitString>(sym, b));  // insert path and symbol into map
   }
